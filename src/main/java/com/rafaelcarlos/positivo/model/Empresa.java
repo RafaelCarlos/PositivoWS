405043dc7b71cda433e8cca409bbef9c0d6811b1
RafaelCarlos <shanglangjunior@hotmail.com>
2015-12-10 14:39:56 -0300

    #1 - Iniciando Repositório

--- src/main/java/com/rafaelcarlos/positivo/model/Empresa.java
+++ src/main/java/com/rafaelcarlos/positivo/model/Empresa.java
@@ -1,14 +1,302 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.rafaelcarlos.positivo.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Objects;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
  * @author rafaellcarloss
  */
-public class Empresa {
-    
+@Entity
+@Table(name = "empresa")
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Empresa implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "razao_social")
+    private String razaoSocial;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 15)
+    @Column(name = "ie")
+    private String iE;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "nome_fantasia")
+    private String nomeFantasia;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "cnpj")
+    private String cnpj;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "endereco")
+    private String endereco;
+
+    @Size(min = 1, max = 5)
+    @Column(name = "numero")
+    private String numero;
+
+    @Size(min = 1, max = 50)
+    @Column(name = "complemento")
+    private String complemento;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 30)
+    @Column(name = "bairro")
+    private String bairro;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "nome_cidade")
+    private String nomeCidade;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 2)
+    @Column(name = "uf")
+    private String uf;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 12)
+    @Column(name = "cep")
+    private String cep;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "telefone_empresa")
+    private String telefoneEmpresa;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 4)
+    @Column(name = "email_empresa")
+    private String emailEmpresa;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "produtoId")
+    private Collection<Produto> produtoCollection;
+
+    @JoinColumn(name = "distribuidora_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Distribuidora distribuidoraId;
+
+    public Empresa() {
+    }
+
+    public Empresa(Integer id) {
+        this.id = id;
+    }
+
+    public Empresa(Integer id, String razaoSocial, String iE, String nomeFantasia, String cnpj, String endereco, String numero, String complemento, String bairro, String nomeCidade, String uf, String cep, String telefoneEmpresa, String emailEmpresa, Collection<Produto> produtoCollection, Distribuidora distribuidoraId) {
+        this.id = id;
+        this.razaoSocial = razaoSocial;
+        this.iE = iE;
+        this.nomeFantasia = nomeFantasia;
+        this.cnpj = cnpj;
+        this.endereco = endereco;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.nomeCidade = nomeCidade;
+        this.uf = uf;
+        this.cep = cep;
+        this.telefoneEmpresa = telefoneEmpresa;
+        this.emailEmpresa = emailEmpresa;
+        this.produtoCollection = produtoCollection;
+        this.distribuidoraId = distribuidoraId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getiE() {
+        return iE;
+    }
+
+    public void setiE(String iE) {
+        this.iE = iE;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getNomeCidade() {
+        return nomeCidade;
+    }
+
+    public void setNomeCidade(String nomeCidade) {
+        this.nomeCidade = nomeCidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getTelefoneEmpresa() {
+        return telefoneEmpresa;
+    }
+
+    public void setTelefoneEmpresa(String telefoneEmpresa) {
+        this.telefoneEmpresa = telefoneEmpresa;
+    }
+
+    public String getEmailEmpresa() {
+        return emailEmpresa;
+    }
+
+    public void setEmailEmpresa(String emailEmpresa) {
+        this.emailEmpresa = emailEmpresa;
+    }
+
+    public Collection<Produto> getProdutoCollection() {
+        return produtoCollection;
+    }
+
+    public void setProdutoCollection(Collection<Produto> produtoCollection) {
+        this.produtoCollection = produtoCollection;
+    }
+
+    public Distribuidora getDistribuidoraId() {
+        return distribuidoraId;
+    }
+
+    public void setDistribuidoraId(Distribuidora distribuidoraId) {
+        this.distribuidoraId = distribuidoraId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Empresa other = (Empresa) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa{" + "id=" + id + ", razaoSocial=" + razaoSocial + ", iE=" + iE + ", nomeFantasia=" + nomeFantasia + ", cnpj=" + cnpj + ", endereco=" + endereco + ", numero=" + numero + ", complemento=" + complemento + ", bairro=" + bairro + ", nomeCidade=" + nomeCidade + ", uf=" + uf + ", cep=" + cep + ", telefoneEmpresa=" + telefoneEmpresa + ", emailEmpresa=" + emailEmpresa + ", produtoCollection=" + produtoCollection + ", distribuidoraId=" + distribuidoraId + '}';
+    }
+
 }
--- src/main/java/com/rafaelcarlos/positivo/model/Produto.java
+++ src/main/java/com/rafaelcarlos/positivo/model/Produto.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.rafaelcarlos.positivo.model;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.Date;
+import java.util.Objects;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -23,6 +20,8 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -32,8 +31,11 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Entity
 @Table(name = "produto")
 @XmlRootElement
-public class Produto {
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Produto implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
@@ -74,7 +76,7 @@ public class Produto {
 
     @Basic(optional = false)
     @NotNull
-    @Column(name = "valor_compra")
+    @Column(name = "modelo_recarga")
     private Integer modeloRecarga;
 
     @Basic(optional = false)
@@ -105,5 +107,177 @@ public class Produto {
     @JoinColumn(name = "empresa_id", referencedColumnName = "id")
     @ManyToOne(optional = false)
     private Empresa empresaId;
+
+    public Produto() {
+    }
+
+    public Produto(Integer id) {
+        this.id = id;
+    }
+
+    public Produto(Integer id, int qtdProduto, String nomeProduto, String codigoOperadora, BigDecimal valorCompra, BigDecimal valorVenda, Integer validade, Integer modeloRecarga, BigDecimal valorMinimo, BigDecimal valorMaximo, BigDecimal valorVariavel, Date ultimaAtualizacao, Collection<Recarga> recargaCollection, Operadora operadoraId, Empresa empresaId) {
+        this.id = id;
+        this.qtdProduto = qtdProduto;
+        this.nomeProduto = nomeProduto;
+        this.codigoOperadora = codigoOperadora;
+        this.valorCompra = valorCompra;
+        this.valorVenda = valorVenda;
+        this.validade = validade;
+        this.modeloRecarga = modeloRecarga;
+        this.valorMinimo = valorMinimo;
+        this.valorMaximo = valorMaximo;
+        this.valorVariavel = valorVariavel;
+        this.ultimaAtualizacao = ultimaAtualizacao;
+        this.recargaCollection = recargaCollection;
+        this.operadoraId = operadoraId;
+        this.empresaId = empresaId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getQtdProduto() {
+        return qtdProduto;
+    }
+
+    public void setQtdProduto(int qtdProduto) {
+        this.qtdProduto = qtdProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getCodigoOperadora() {
+        return codigoOperadora;
+    }
+
+    public void setCodigoOperadora(String codigoOperadora) {
+        this.codigoOperadora = codigoOperadora;
+    }
+
+    public BigDecimal getValorCompra() {
+        return valorCompra;
+    }
+
+    public void setValorCompra(BigDecimal valorCompra) {
+        this.valorCompra = valorCompra;
+    }
+
+    public BigDecimal getValorVenda() {
+        return valorVenda;
+    }
+
+    public void setValorVenda(BigDecimal valorVenda) {
+        this.valorVenda = valorVenda;
+    }
+
+    public Integer getValidade() {
+        return validade;
+    }
+
+    public void setValidade(Integer validade) {
+        this.validade = validade;
+    }
+
+    public Integer getModeloRecarga() {
+        return modeloRecarga;
+    }
+
+    public void setModeloRecarga(Integer modeloRecarga) {
+        this.modeloRecarga = modeloRecarga;
+    }
+
+    public BigDecimal getValorMinimo() {
+        return valorMinimo;
+    }
+
+    public void setValorMinimo(BigDecimal valorMinimo) {
+        this.valorMinimo = valorMinimo;
+    }
+
+    public BigDecimal getValorMaximo() {
+        return valorMaximo;
+    }
+
+    public void setValorMaximo(BigDecimal valorMaximo) {
+        this.valorMaximo = valorMaximo;
+    }
+
+    public BigDecimal getValorVariavel() {
+        return valorVariavel;
+    }
+
+    public void setValorVariavel(BigDecimal valorVariavel) {
+        this.valorVariavel = valorVariavel;
+    }
+
+    public Date getUltimaAtualizacao() {
+        return ultimaAtualizacao;
+    }
+
+    public void setUltimaAtualizacao(Date ultimaAtualizacao) {
+        this.ultimaAtualizacao = ultimaAtualizacao;
+    }
+
+    public Collection<Recarga> getRecargaCollection() {
+        return recargaCollection;
+    }
+
+    public void setRecargaCollection(Collection<Recarga> recargaCollection) {
+        this.recargaCollection = recargaCollection;
+    }
+
+    public Operadora getOperadoraId() {
+        return operadoraId;
+    }
+
+    public void setOperadoraId(Operadora operadoraId) {
+        this.operadoraId = operadoraId;
+    }
+
+    public Empresa getEmpresaId() {
+        return empresaId;
+    }
+
+    public void setEmpresaId(Empresa empresaId) {
+        this.empresaId = empresaId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 89 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Produto other = (Produto) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" + "id=" + id + ", qtdProduto=" + qtdProduto + ", nomeProduto=" + nomeProduto + ", codigoOperadora=" + codigoOperadora + ", valorCompra=" + valorCompra + ", valorVenda=" + valorVenda + ", validade=" + validade + ", modeloRecarga=" + modeloRecarga + ", valorMinimo=" + valorMinimo + ", valorMaximo=" + valorMaximo + ", valorVariavel=" + valorVariavel + ", ultimaAtualizacao=" + ultimaAtualizacao + ", recargaCollection=" + recargaCollection + ", operadoraId=" + operadoraId + ", empresaId=" + empresaId + '}';
+    }
 
 }
