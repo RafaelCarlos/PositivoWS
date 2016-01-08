6ac82c17b00ac7bb43938b33277a8bc27a1a5e22
RafaelCarlos <shanglangjunior@hotmail.com>
2016-01-08 08:05:10 -0300

    #5- Criação de uma classe conversora e outra para criptografia.

---
+++ src/main/java/com/rafaelcarlos/positivo/util/Conversor.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rafaelcarlos.positivo.util;
+
+//import java.io.IOException;
+//import java.io.StringReader;
+//import javax.xml.parsers.DocumentBuilder;
+//import javax.xml.parsers.DocumentBuilderFactory;
+//import javax.xml.parsers.ParserConfigurationException;
+//import org.xml.sax.SAXException;
+//
+//import org.xml.sax.ErrorHandler;
+//import org.xml.sax.SAXParseException;
+//import org.w3c.dom.Document;
+//import org.w3c.dom.DocumentType;
+//import org.w3c.dom.Entity;
+//import org.w3c.dom.NamedNodeMap;
+//import org.w3c.dom.Node;
+
+/**
+ *
+ * @author rafaellcarloss
+ */
+public class Conversor {
+
+//    private void parseXML(String xml) throws ParserConfigurationException, SAXException, IOException {
+//        InputSource is = new InputSource(new StringReader(xml));
+//        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+//        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+//        Document doc = dBuilder.parse(is);
+//
+//        doc.getDocumentElement().normalize();
+//        String lugar = doc.getElementsByTagName("geoplugin_place").item(0).getTextContent();
+//        System.out.println(lugar);
+//
+//        String regiao = doc.getElementsByTagName("geoplugin_region").item(0).getTextContent();
+//        System.out.println(regiao);
+//
+//        String pais = doc.getElementsByTagName("geoplugin_countryCode").item(0).getTextContent();
+//        System.out.println(pais);
+//    }
+}
